ba6626dfb1766807a6db1e55e9fa4831f029366a
Lars Hupel <lars.hupel@mytum.de>
2018-10-05 18:03:40 +0200

    set binaries executable

--- get-opam.sh
+++ get-opam.sh
@@ -6,4 +6,6 @@ curl -sLo "bin/opam-2.0.0-x86_64-linux" "https://github.com/ocaml/opam/releases/
 curl -sLo "bin/opam-2.0.0-x86-linux" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-i686-linux"
 curl -sLo "bin/opam-2.0.0-x86_64-darwin" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-darwin"
 
+chmod +x bin/*
+
 sha256sum --quiet -c checksums || (echo "Failed to verify files"; exit 1)
