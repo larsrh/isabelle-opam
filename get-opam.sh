1fcc726088d0e9161a261920f45535bed9e071fe
Lars Hupel <lars.hupel@mytum.de>
2018-10-05 17:36:02 +0200

    initial version of Isabelle/opam component

---
+++ get-opam.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+curl -sLo "bin/opam-2.0.0-x86_64-linux" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-linux"
+curl -sLo "bin/opam-2.0.0-x86-linux" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-i686-linux"
+curl -sLo "bin/opam-2.0.0-x86_64-darwin" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-darwin"
+
+sha256sum --quiet -c checksums || (echo "Failed to verify files"; exit 1)
