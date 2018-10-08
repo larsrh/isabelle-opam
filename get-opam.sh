dbc7cc4fe7e09917a66f349e532a63b9f4bd8acd
Lars Hupel <lars.hupel@mytum.de>
2018-10-08 16:13:20 +0200

    kill x86

--- get-opam.sh
+++ get-opam.sh
@@ -3,7 +3,6 @@
 set -ex
 
 curl -sLo "bin/opam-2.0.0-x86_64-linux" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-linux"
-curl -sLo "bin/opam-2.0.0-x86-linux" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-i686-linux"
 curl -sLo "bin/opam-2.0.0-x86_64-darwin" "https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-darwin"
 
 chmod +x bin/*
